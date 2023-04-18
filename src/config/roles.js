1f5d17ae579f49642b6f5dc58d23b9ab35dd0947
Maruthi Saikumar <Maruthi@noodle.digital>
2023-04-18 15:21:52 +0530

    Frist commit

---
+++ src/config/roles.js
@@ -0,0 +1,10 @@
+export const ROLES = {
+    EMPLOYEE: 'EMPLOYEE',
+    CEO: 'CEO',
+    ADMIN: 'ADMIN',
+    MANAGER: 'MANAGER',
+    GUARD: 'GUARD',
+    INCHARGE: 'INCHARGE',
+    USER: 'USER',
+    LEADER: 'LEADER',
+}
